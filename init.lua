7b137c3e8dbdaa19dd50a7982f517556ceb386df
DS-Minetest <vorunbekannt75@web.de>
2017-02-28 13:45:05 +0100

    add load timer

--- init.lua
+++ init.lua
@@ -7,8 +7,20 @@
       \/          \/     \/        \/          \/     \/
 ]]
 
+local load_time_start = os.clock()
+
+
 local path = minetest.get_modpath("mvehicles")
 
 dofile(path.."/tank.lua")
 dofile(path.."/truck.lua")
 dofile(path.."/construction.lua")
+
+
+local time = math.floor(tonumber(os.clock()-load_time_start)*100+0.5)/100
+local msg = "[mvehicles] loaded after ca. "..time
+if time > 0.05 then
+	print(msg)
+else
+	minetest.log("info", msg)
+end
